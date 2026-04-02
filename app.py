fa9baafee65b9572f49f07944da7fe97ef24d056
Victor <0327victor@gmail.com>
2026-04-02 20:53:37 +0800

    新增功能

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
+import json
+import sys
+import requests
+# Import blinka python modules.
+import board
+import digitalio
+import RPi.GPIO as GPIO  # 引入 RPi.GPIO 库
+import time
+import socket
 from flask import Flask, request, abort
 import os
 from linebot import LineBotApi, WebhookHandler
@@ -45,8 +54,28 @@ def handle_message(event):
     # v3 版獲取文字的方式
     user_text = event.message.text
     
-    print(f"發言者 ID: {user_id}", flush=True)
-    print(f"文字內容: {user_text}", flush=True)
+    with open("member.json", "r", encoding="utf-8") as f:
+        member = json.load(f)
+
+    if (user_text=="addmember") :
+        if (user_id not in member['member']):
+            member['member'].append(user_id)
+            with open("member.json", "w", encoding="utf-8") as f:
+                json.dump(member, f, indent=4, ensure_ascii=False)
+    else:
+        if user_id in member['member']:
+            if user_text=='openair':
+                try:
+                    import gpio
+                    gpio.click()
+                except:
+                    pass
+            if user_text=='openlight':
+                try:
+                    import gpio
+                    gpio.socketsub()
+                except:
+                    pass
 if __name__ == "__main__":
     # Flask 預設跑在 5000 埠
     app.run(port=5000)
